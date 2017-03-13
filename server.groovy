91398a8068e611627b37a0fcd6b995e2c88568d1
Sridhar Sarnobat <sridhar.sarnobat@itsoninc.com>
2017-03-13 15:29:27 -0700

    Minor changes (untested)

--- server.groovy
+++ server.groovy
@@ -10,15 +10,14 @@ import org.apache.commons.cli.ParseException;
 public class CommandLineOptionsExample {
 
   public static void main(String[] args) {
-    System.out.println("CommandLineOptionsExample.main() - SRIDHAR: 1");
 
     Options options = new Options()
-        // .addOption("p", "port", true, "Here you can set parameter .")
+        .addOption("p", "port", true, "Here you can set parameter .")
         .addOption("h", "help", false, "show help.");
 
-    options.addOption(Option.builder("p").longOpt("port").required().build());
+    // This doesn't work
+    //options.addOption(Option.builder("p").argName("p").longOpt("port").required().build());
 
-    System.out.println("CommandLineOptionsExample.main() - SRIDHAR: 2");
     try {
       CommandLine cmd = new DefaultParser().parse(options, args);
       parse(options, cmd);
@@ -29,21 +28,11 @@ public class CommandLineOptionsExample {
 
   private static void parse(Options options, CommandLine cmd) {
     String port = cmd.getOptionValue("p", "4444");
-    String recursive = cmd.getOptionValue("r", "false");
     System.out.println("CommandLineOptionsExample.parse() - SRIDHAR: port = " + port);
-    System.out.println("CommandLineOptionsExample.parse() - SRIDHAR: recursive = " + recursive);
 
     if (cmd.hasOption("h")) {
       help(options);
     }
-
-    // if (cmd.hasOption("v")) {
-    // System.err.println("Using cli argument -v=" + cmd.getOptionValue("v"));
-    // // Whatever you want to do with the setting goes here
-    // } else {
-    // System.err.println("Missing v option");
-    // help(options);
-    // }
   }
 
   private static void help(Options options) {
