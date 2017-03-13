b460e39212ed2ffaa48c01257dc23a4d96af2836
Sridhar Sarnobat <sridhar.sarnobat@itsoninc.com>
2017-03-13 15:19:39 -0700

    Minor changes (untested)

--- server.groovy
+++ server.groovy
@@ -15,8 +15,8 @@ public class CommandLineOptionsExample {
     Options options = new Options();
     options.addOption("h", "help", false, "show help.");
     Option.Builder builder = Option.builder("p");
-    Option p = builder.argName("p").longOpt("port").build();
-    //options.addOption("p", "port", true, "Here you can set parameter .");
+    Option p = builder.argName("p").longOpt("port").required().build();
+    // options.addOption("p", "port", true, "Here you can set parameter .");
     options.addOption(p);
 
     System.out.println("CommandLineOptionsExample.main() - SRIDHAR: 2");
@@ -29,21 +29,20 @@ public class CommandLineOptionsExample {
   }
 
   private static void parse(Options options, CommandLine cmd) {
-    String port = // options.getOption("p").getValue("4444");
-        cmd.getOptionValue("p", "4444");
+    String port = cmd.getOptionValue("p", "4444");
     System.out.println("CommandLineOptionsExample.parse() - SRIDHAR: port = " + port);
 
     if (cmd.hasOption("h")) {
       help(options);
     }
 
-//    if (cmd.hasOption("v")) {
-//      System.err.println("Using cli argument -v=" + cmd.getOptionValue("v"));
-//      // Whatever you want to do with the setting goes here
-//    } else {
-//      System.err.println("Missing v option");
-//      help(options);
-//    }
+    // if (cmd.hasOption("v")) {
+    // System.err.println("Using cli argument -v=" + cmd.getOptionValue("v"));
+    // // Whatever you want to do with the setting goes here
+    // } else {
+    // System.err.println("Missing v option");
+    // help(options);
+    // }
   }
 
   private static void help(Options options) {
