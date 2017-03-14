9f1fa05dafb8326e54a2e30740c6343f324a4223
Sridhar Sarnobat <sridhar.sarnobat@itsoninc.com>
2017-03-13 17:31:26 -0700

    Minor changes (untested)

--- server.groovy
+++ server.groovy
@@ -3,43 +3,36 @@ package test;
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.DefaultParser;
 import org.apache.commons.cli.HelpFormatter;
-import org.apache.commons.cli.Option;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 
 public class CommandLineOptionsExample {
 
   public static void main(String[] args) {
+    String port;
+    _parseOptions: {
+      Options options = new Options()
+          .addOption("p", "port", true, "Here you can set parameter .")
+          .addOption("h", "help", false, "show help.");
 
-    Options options = new Options()
-        .addOption("p", "port", true, "Here you can set parameter .")
-        .addOption("h", "help", false, "show help.");
+      // This doesn't work
+      // options.addOption(Option.builder("p").argName("p").longOpt("port").required().build());
 
-    // This doesn't work
-    //options.addOption(Option.builder("p").argName("p").longOpt("port").required().build());
+      try {
+        CommandLine cmd = new DefaultParser().parse(options, args);
+        port = cmd.getOptionValue("p", "4444");
+        System.out.println("CommandLineOptionsExample.parse() - SRIDHAR: port = " + port);
 
-    try {
-      CommandLine cmd = new DefaultParser().parse(options, args);
-      parse(options, cmd);
-    } catch (ParseException e) {
-      e.printStackTrace();
+        if (cmd.hasOption("h")) {
+          // This prints out some help
+          HelpFormatter formater = new HelpFormatter();
+
+          formater.printHelp("Main", options);
+          System.exit(0);
+        }
+      } catch (ParseException e) {
+        e.printStackTrace();
+      }
     }
-  }
-
-  private static void parse(Options options, CommandLine cmd) {
-    String port = cmd.getOptionValue("p", "4444");
-    System.out.println("CommandLineOptionsExample.parse() - SRIDHAR: port = " + port);
-
-    if (cmd.hasOption("h")) {
-      help(options);
-    }
-  }
-
-  private static void help(Options options) {
-    // This prints out some help
-    HelpFormatter formater = new HelpFormatter();
-
-    formater.printHelp("Main", options);
-    System.exit(0);
   }
 }
