bba013caf2bb65fc85b2a37bdaf65bc51e476a81
Sridhar Sarnobat <sridhar.sarnobat@itsoninc.com>
2017-03-14 11:15:18 -0700

    Minor changes (untested)

---
+++ run.sh
@@ -0,0 +1,2 @@
+# This works in java but not groovy
+groovy server.groovy -a 1024 --port 8080
--- server.groovy
+++ server.groovy
@@ -3,7 +3,7 @@ package test;
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.DefaultParser;
 import org.apache.commons.cli.HelpFormatter;
-import org.apache.commons.cli.Options;
+import org.apache.commons.cli.*;
 import org.apache.commons.cli.ParseException;
 
 public class CommandLineOptionsExample {
@@ -11,9 +11,13 @@ public class CommandLineOptionsExample {
   public static void main(String[] args) {
     String port;
     _parseOptions: {
-      Options options = new Options()
-          .addOption("p", "port", true, "Here you can set parameter .")
-          .addOption("h", "help", false, "show help.");
+
+      Options options = new Options().addOption("p", "port", true, "Here you can set parameter .").addOption("h",
+          "help", false, "show help.");
+
+      Option option = Option.builder("a").longOpt("block-size").desc("use SIZE-byte blocks").hasArg().argName("SIZE")
+          .build();
+      options.addOption(option);
 
       // This doesn't work
       // options.addOption(Option.builder("p").argName("p").longOpt("port").required().build());
@@ -22,6 +26,7 @@ public class CommandLineOptionsExample {
         CommandLine cmd = new DefaultParser().parse(options, args);
         port = cmd.getOptionValue("p", "4444");
         System.out.println("CommandLineOptionsExample.parse() - SRIDHAR: port = " + port);
+        System.out.println("CommandLineOptionsExample.parse() - SRIDHAR: a = " + cmd.getOptionValue("a"));
 
         if (cmd.hasOption("h")) {
           // This prints out some help
